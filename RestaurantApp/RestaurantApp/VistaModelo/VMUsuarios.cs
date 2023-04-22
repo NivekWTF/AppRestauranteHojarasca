4089d84dd75ad939ff05288cacfb34b59bf58ed2
NivekWTF <nivekgamer2015@gmail.com>
2023-04-22 11:52:06 -0700

    Se crean funciones clicked para cada Botón

--- RestaurantApp/RestaurantApp/VistaModelo/VMUsuarios.cs
+++ RestaurantApp/RestaurantApp/VistaModelo/VMUsuarios.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 using Xamarin.Forms;
+using RestaurantApp.Modelo;
 
 namespace RestaurantApp.VistaModelo
 {
@@ -22,6 +23,27 @@ namespace RestaurantApp.VistaModelo
             catch (Exception ex)
             {
                 Application.Current.MainPage.DisplayAlert("Error", ex.StackTrace, "Ok");
+            }
+            finally
+            {
+                CONEXION_MAESTRA.Cerrar();
+            }
+        }
+
+        public void validarUsuarios(MUsuarios parametros, ref int id)
+        {
+            try
+            {
+                CONEXION_MAESTRA.abrir();
+                SqlCommand cmd = new SqlCommand("validarUsuario", CONEXION_MAESTRA.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@password", parametros.Password);
+                cmd.Parameters.AddWithValue("@login", parametros.Login);
+                id = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                id = 0;
             }
             finally
             {
--- RestaurantApp/RestaurantApp/Vistas/MarcarContraseñas.xaml.cs
+++ RestaurantApp/RestaurantApp/Vistas/MarcarContraseñas.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestaurantApp.Modelo;
+using RestaurantApp.VistaModelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,93 @@ namespace RestaurantApp.Vistas
         public MarcarContraseñas()
         {
             InitializeComponent();
+        }
+
+        public string login;
+
+        private void btn7_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "7";
+        }
+
+        private void btn8_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "8";
+        }
+
+        private void btn9_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "9";
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Clicked(object sender, EventArgs e)
+        {
+            int contador;
+            contador = txtContraseña.Text.Count();
+            if (contador != 0)
+            {
+                txtContraseña.Text = txtContraseña.Text.Substring(0, txtContraseña.Text.Count() - 1);
+            }
+        }
+
+        private void btn0_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "0";
+        }
+
+        private void btnBorrar_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text = "";
+        }
+
+        private void btn3_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "3";
+        }
+
+        private void btn2_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "2";
+        }
+
+        private void btn1_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "1";
+        }
+
+        private void btn6_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "6";
+        }
+
+        private void btn5_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "5";
+        }
+
+        private void btn4_Clicked(object sender, EventArgs e)
+        {
+            txtContraseña.Text += "4";
+        }
+
+        private void txtContraseña_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void validarContraseña()
+        {
+            MUsuarios parametros = new MUsuarios();
+            VMUsuarios funcion = new VMUsuarios();
+
+            parametros.Password = txtContraseña.Text;
+            parametros.Login = login;
+
         }
     }
 }
